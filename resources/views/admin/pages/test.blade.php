69dc792dce7aab87325fb81df2637456b5050e91
Kh-Ulugbek <kh9ulugbek@gmail.com>
2022-06-02 15:52:26 +0500

    apelsin

--- resources/views/admin/pages/test.blade.php
+++ resources/views/admin/pages/test.blade.php
@@ -33,6 +33,36 @@
                         <button type="submit" class="click_logo"><i></i>Оплатить через CLICK</button>
                     </form>
                 </div>
+                <div>
+                    <form method="GET" action="https://oplata.kapitalbank.uz">
+                        {{--                    // Обязательные поля--}}
+                        <input type="hidden" name="cash" value="f81c68ccf43c462e8334d22b2cb04ce9"/>
+                        {{--                    // уникальный Cash ID (выдается Апельсином)--}}
+                        <input type="hidden" name="redirectUrl" value="http://dacha/test"/>
+                        {{--                    // URL для отправки на сайт после оплаты--}}
+                        <input type="hidden" name="description" value="TEST"/>
+                        {{--                    // фраза, который выйдет на экране оплаты у Пользователя, например «Пополнение--}}
+                        {{--                    платежа на www._______.uz»--}}
+                        {{--                    // Предусматривается установка дополнительных полей для Клиента. После--}}
+                        {{--                    проведения платежа, данные по полям будут отправляться на ваш биллинг URL в формате--}}
+                        {{--                    JSON.--}}
+                        {{--                    // В JSON не будет required/optional полей--}}
+                        {{--                    // В JSON будут поля amount (сумма оплаты Пользователя) и transactionId (уникальный--}}
+                        {{--                    айди транзакции)--}}
+                        <input type="hidden" name="amount" value="1000"/>
+                        {{--                        <input type="hidden" name="" value=""/>--}}
+                        {{--                        <input type="hidden" name="" value=""/>--}}
+                        {{--                        ...--}}
+                        <button type="submit"
+                                style="cursor: pointer;
+border: 1px solid #ebebeb; border-radius: 6px;
+background: linear-gradient(to top, #f1f2f2, white);
+width: 100px; height: 54px; display: flex;
+align-items: center; justify-content: center;">
+                            <img style="width: 100px; height: 42px;" src="https://oplata.kapitalbank.uz/images/apelsin-v1.png">
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
